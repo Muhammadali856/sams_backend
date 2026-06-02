af25b64f7b874ff03c3ca7a337b49f7192000bbe
Muhammadali856 <eshboevmuhammadali2@gmail.com>
2026-06-02 12:56:23 +0800

    added headteacher logic 1.1

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, generics, status, permissions
+from rest_framework import viewsets, generics, status, permissions, api_view
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Subject, Assignment, Task, Student, Teacher, Quiz
