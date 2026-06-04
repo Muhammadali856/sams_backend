61d885bbe93d4faf159d92b48eb9ce7ee9d80352
Muhammadali856 <eshboevmuhammadali2@gmail.com>
2026-06-04 18:29:31 +0800

    Added forgot password logic 1.6

--- core/settings.py
+++ core/settings.py
@@ -123,5 +123,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'eshboevmuhammadali2@gmail.com' 
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'eshboevmuhammadali2@gmail.com'
